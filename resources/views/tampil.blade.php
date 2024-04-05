dd99a3064967ee09800962b51ead6be00ddbdbdd
aminkurniyati <amin.kurniyati@gmail.com>
2024-04-05 19:55:13 +0700

    Laravel CRUD AJAX

---
+++ resources/views/tampil.blade.php
@@ -0,0 +1,37 @@
+@extends('template')
+
+@section('main')
+    <main>
+        <div class="container-fluid px-4">
+            <h1 class="mt-4">Dashboard</h1>
+            <ol class="breadcrumb mb-4">
+                <li class="breadcrumb-item active"><a href="/">Dashboard</a></li>
+            </ol>
+            <div class="row">
+                {{-- table --}}
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">No</th>
+                            <th scope="col">Judul</th>
+                            <th scope="col">Berita</th>
+                            <th scope="col">Penulis</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row"></th>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+
+        </div>
+    </main>
+@endsection
